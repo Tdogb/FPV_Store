ece63efe129c8eed9cd9ab948c53431e288ef1b3
Tdogb <tdogbeard@gmail.com>
2019-03-05 14:21:19 -0500

    orders

--- FPV_Store/views.py
+++ FPV_Store/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.db import models
 
 def homePage(request):
     #return HttpResponse('homepage')
--- products/models.py
+++ products/models.py
@@ -10,6 +10,16 @@ class Product(models.Model):
     in_stock = models.BooleanField()
     description = models.TextField()
     thumb = models.ImageField(default='/media/default.png')
+    S800 = 's800'
+    GEN = 'general'
+    PRODUCT_TYPES = (
+        (S800, 's800'),
+        (GEN, 'general'),
+    )
+    product_type = models.CharField(default=GEN, choices=PRODUCT_TYPES, max_length=50)
+
+    def getProductTypes(self):
+        return self.PRODUCT_TYPES
 
     def __str__(self):
         return self.name
